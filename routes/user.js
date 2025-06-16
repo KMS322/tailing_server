b7d1b89ee36309c30a058a2cbd70f9f2c2bcf043
KMS322 <kms930322@naver.com>
2025-06-16 16:25:55 +0900

    250616_05

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,9 @@ const { isLoggedIn, isNotLoggedIn } = require("../middlewares/auth");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const admin = require("firebase-admin");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 router.post("/signup", isNotLoggedIn, async (req, res, next) => {
   try {
@@ -204,7 +207,19 @@ router.post("/check/password", async (req, res, next) => {
   }
 });
 
-const serviceAccount = require("../config/talktail-872bf-firebase-adminsdk-fbsvc-e4f7881bfe.json");
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: "googleapis.com"
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
